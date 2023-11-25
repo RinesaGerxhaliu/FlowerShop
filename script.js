7cd91dcf22f6e94c38e8a681b58eccabd3a16ab3
RinesaGerxhaliu <rg64753@ubt-uni.net>
2023-11-25 23:17:37 +0100

    slideri

--- script.js
+++ script.js
@@ -1,46 +1,25 @@
-var currentImg = 0;
-var imgs = document.querySelectorAll('.slider img');
-let dots = document.querySelectorAll('.dot');
-var interval = 3000;
+let slideIndex = 0;
 
+    function showSlide(index) {
+        const slider = document.getElementById('slider');
+        const slides = document.getElementsByClassName('slide');
 
-var secondEventTitle = 'Hi! *Freshmen* Orientation Day';
+        if (index >= slides.length) {
+            slideIndex = 0;
+        } else if (index < 0) {
+            slideIndex = slides.length - 1;
+        } else {
+            slideIndex = index;
+        }
 
-var thirdEventDate = new Date('2023-02-01'); // pull this from database
-var thirdEventDateString = thirdEventDate.toLocaleDateString('en-us', { year: 'numeric', month: 'short', day: 'numeric' });
-var days = calculateDays(new Date(), thirdEventDate) || 0;
-const countdownText = days > 0 ? `${days} days left` : 'Live Now!';
+        const translateValue = -slideIndex * 100 + '%';
+        slider.style.transform = 'translateX(' + translateValue + ')';
+    }
 
-var secondImageUrl = `https://ondemand.bannerbear.com/simpleurl/01YWAxB7nGYdJrKoXM/title/text/${encodeURIComponent(secondEventTitle)}`;
-var thirdImageUrl = `https://ondemand.bannerbear.com/simpleurl/ley9O0B2ZGbB4GjRDY/date/text/${encodeURIComponent(
-  thirdEventDateString
-)}/countdown/text/${encodeURIComponent(countdownText)}`;
+    function prevSlide() {
+        showSlide(slideIndex - 1);
+    }
 
-document.getElementById('img-2').src = secondImageUrl;
-document.getElementById('img-3').src = thirdImageUrl;
-
-var timer = setInterval(changeSlide, interval);
-
-function changeSlide(n) {
-  for (var i = 0; i < imgs.length; i++) {
-    imgs[i].style.opacity = 0;
-    dots[i].className = dots[i].className.replace(' active', '');
-  }
-
-  currentImg = (currentImg + 1) % imgs.length;
-
-  if (n != undefined) {
-    clearInterval(timer);
-    timer = setInterval(changeSlide, interval);
-    currentImg = n;
-  }
-
-  imgs[currentImg].style.opacity = 1;
-  dots[currentImg].className += ' active';
-}
-
-function calculateDays(today, eventDate) {
-  const difference = eventDate.getTime() - today.getTime();
-
-  return Math.ceil(difference / (1000 * 3600 * 24)); // convert to days
-}
+    function nextSlide() {
+        showSlide(slideIndex + 1);
+    }
