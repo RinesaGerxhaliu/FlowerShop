a15e1516cc43c942a5f1b37b12a02cb4b5aff892
RinesaGerxhaliu <rg64753@ubt-uni.net>
2023-12-08 17:55:04 +0100

    finisheddd

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
 let slideIndex = 0;
 
-    function showSlide(index) {
-        const slider = document.getElementById('slider');
-        const slides = document.getElementsByClassName('slide');
+function showSlide(index) {
+  const slider = document.getElementById("slider");
+  const slides = document.getElementsByClassName("slide");
 
-        if (index >= slides.length) {
-            slideIndex = 0;
-        } else if (index < 0) {
-            slideIndex = slides.length - 1;
-        } else {
-            slideIndex = index;
-        }
+  if (index >= slides.length) {
+    slideIndex = 0;
+  } else if (index < 0) {
+    slideIndex = slides.length - 1;
+  } else {
+    slideIndex = index;
+  }
 
-        const translateValue = -slideIndex * 100 + '%';
-        slider.style.transform = 'translateX(' + translateValue + ')';
-    }
+  const translateValue = -slideIndex * 100 + "%";
+  slider.style.transform = "translateX(" + translateValue + ")";
+}
 
-    function prevSlide() {
-        showSlide(slideIndex - 1);
-    }
+function prevSlide() {
+  showSlide(slideIndex - 1);
+}
 
-    function nextSlide() {
-        showSlide(slideIndex + 1);
-    }
+function nextSlide() {
+  showSlide(slideIndex + 1);
+}
+
+document.getElementById('mobile-menu').addEventListener('click', function() {
+    document.querySelector('.listat').classList.toggle('show');
+});
